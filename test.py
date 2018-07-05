55e5f4c9bcc714c4e5615845f3fde0775edfbcc5
mnsumant21 <mnsumanth0321@gmail.com>
2018-07-05 13:29:38 +0530

    Saved to a file

--- test.py
+++ test.py
@@ -9,12 +9,23 @@ root.filename =  filedialog.askopenfilename(initialdir = "C:/Users/SUMANTH/Deskt
 with open(root.filename, "rb") as file_obj:
 	content = file_obj.read()
 	b = str(binascii.hexlify(content))
-print(b)
+#print(b)
 
-print(len(b))
+#print(len(b))
 
 for i in range(0,len(b)-1,2):
 	s=(b[i]+b[i+1])
 	list.append(s)
-print(list)
+del list[0]
+#print(list)
+file=open("file.txt",'w')
+for i in range(0,len(list),16):
+	#print(list[i:i+16])
+	file.write(str(list[i:i+16]))
+	file.write('\n\n')
+file.close()
+	
+	
+	
+
 	
