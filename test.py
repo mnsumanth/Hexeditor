aa2f538a4a97932eb0fe93f5097d88e277c8774a
mnsumant21 <mnsumanth0321@gmail.com>
2018-07-05 11:27:08 +0530

    Added to list

--- test.py
+++ test.py
@@ -3,10 +3,18 @@ from tkinter import filedialog
 import binascii
 
 root=Tk()
+list=[]
 input("Press enter to input a file")
 root.filename =  filedialog.askopenfilename(initialdir = "C:/Users/SUMANTH/Desktop/",title = "Select file")
 with open(root.filename, "rb") as file_obj:
 	content = file_obj.read()
-	b = binascii.hexlify(content)
-
+	b = str(binascii.hexlify(content))
 print(b)
+
+print(len(b))
+
+for i in range(0,len(b)-1,2):
+	s=(b[i]+b[i+1])
+	list.append(s)
+print(list)
+	
